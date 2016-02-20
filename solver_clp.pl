aa658d0b312aef38cfe75f5be4014f1c46b7be64
opocaj92 <jacopo.castellini@gmail.com>
2016-02-20 19:07:37 +0100

    Corretta soluzione CLP, aggiunte istanza.

--- solver_clp.pl
+++ solver_clp.pl
@@ -8,14 +8,22 @@ get_people(_,[],[]).
 get_people(X,[[Y,Z]|T],R):-X==Y,get_people(X,T,R1),R=[Z|R1].
 get_people(X,[[Y,_]|T],R):-X\=Y,get_people(X,T,R).
 
-solve(C,L,R):-length(L,N),length(R,N),R ins 0..1,constraints(C,R,L,L),sum(R,#=,V),labeling([max(V)],R).
+solve(C,L,R):-length(L,N),length(R,N),R ins 0..1,constraints(C,R,R,L,L),sum(R,#=,V),labeling([max(V)],R).
 
-constraints(_,[],[],_).
-constraints(C,[A|R],[[X,Y]|T],L):-check(C,A,X,Y,L),constraints(C,R,T,L).
+constraints(_,[],_,[],_).
+constraints(C,[A|R],Z,[[X,Y]|T],L):-check(C,A,Z,X,Y,L),constraints(C,R,Z,T,L).
 
-check(C,A,X,Y,L):-J#=X+1,checkpoint(J,C),not_present(J,Y,L),A#=0.
-check(C,A,X,Y,L):-J#=X-1,checkpoint(J,C),not_present(J,Y,L),A#=0.
-check(_,_,_,_,_).
+check(C,A,Z,X,Y,L):-J is X+1,checkpoint(J,C),not_present(J,Y,L),V is X+1,constraint_prev(V,Y,Z,L,Z,L),W is X-1,constraint_next(W,Y,Z,L,Z,L),A#=0.
+check(C,A,Z,X,Y,L):-J is X-1,checkpoint(J,C),not_present(J,Y,L),V is X+1,constraint_prev(V,Y,Z,L,Z,L),W is X-1,constraint_next(W,Y,Z,L,Z,L),A#=0.
+check(_,_,_,_,_,_).
+
+constraint_next(_,_,[],[],_,_).
+constraint_next(X,Y,[A|_],[[Z,W]|_],P,T):-X==Z,Y==W,A#=0,V=X+1,constraint_next(V,Y,P,T,P,T).
+constraint_next(X,Y,[_|R],[[_,_]|L],P,T):-constraint_next(X,Y,R,L,P,T).
+
+constraint_prev(_,_,[],[],_,_).
+constraint_prev(X,Y,[A|_],[[Z,W]|_],P,T):-X==Z,Y==W,A#=0,V=X-1,constraint_next(V,Y,P,T,P,T).
+constraint_prev(X,Y,[_|R],[[_,_]|L],P,T):-constraint_next(X,Y,R,L,P,T).
 
 xj(C,L,R):-solve(C,L,V),get_list(L,V,R).
 
